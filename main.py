b16f87a69565250b25a15e0dd46a7be6effefb22
Anagha Ravinarayan <aravinarayan@salesforce.com>
2022-08-15 11:45:16 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL_SMTP_SERVER = "YOUR EMAIL PROVIDER SMTP SERVER ADDRESS"
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
+MY_NAME = "YOUR NAME"
+
+today = (datetime.now().month, datetime.now().day)
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+        contents = contents.replace("[MY_NAME]", MY_NAME)
+
+    with smtplib.SMTP(MY_EMAIL_SMTP_SERVER) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Automated birthday Wisher
+
+This Python app lets you wish your loved ones on their birthdays using an automated program.
+
+### Steps to take before running this program
+1. Change MY_EMAIL, MY_PASSWORD, MY_EMAIL_SMTP_SERVER, MY_NAME to your own details.
+2. Go to your email provider and make it allow less secure apps.
+3. Update birthdays.csv to contain the details of your close ones. Alternatively you can use today's month and day to test this code.
+4. Optionally, you can configure a job on a python app host in the cloud such as [pythonanywhere](https://www.pythonanywhere.com/) and schedule a daily job run for this program.
+
+### External Libraries
+1. [pandas](https://pandas.pydata.org/)
